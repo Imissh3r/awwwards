25482a30d4f6dea454d3cfbf5eaf92df811fcc85
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-11-21 17:31:43 +0000

    Fix vercel analytics on layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Story from './components/Story';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
